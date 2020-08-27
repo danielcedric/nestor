2b200aba3a7caa9ea7f4eb0e4581273a6c146f99
cdaniel <cedric.daniel@outlook.com>
2020-08-27 21:38:08 +0200

    * GenericTypeExtensions.cs: * IdentifiedCommand.cs: * Nestor.Tools.Application.csproj: * IdentifiedCommand.cs:
    * Nestor.Tools.csproj: Ajout de la classe d'extensions
      GenericTypeExtensions

--- Nestor.Tools.Application/Commands/IdentifiedCommand.cs
+++ Nestor.Tools.Application/Commands/IdentifiedCommand.cs
@@ -3,16 +3,23 @@ using MediatR;
 
 namespace Nestor.Tools.Application.Commands
 {
-    public class IdentifiedCommand<TCommand, TResponse> : IRequest<TResponse>
-        where TCommand : IRequest<TResponse>
+    /// <summary>
+    /// Représente une commande identifiée
+    /// </summary>
+    /// <typeparam name="TCommand">Type du gestionnaire de commandes qui exécute l'opération si la demande n'est pas dupliquée</typeparam>
+    /// <typeparam name="TResult">Valeur de retour du gestionnaire de commandes interne</typeparam>
+    public class IdentifiedCommand<TCommand, TResult> : IRequest<TResult>
+        where TCommand : IRequest<TResult>
     {
+        #region Properties
+        public TCommand Command { get; }
+        public Guid Id { get; }
+        #endregion
+
         public IdentifiedCommand(TCommand command, Guid id)
         {
             Command = command;
             Id = id;
         }
-
-        public TCommand Command { get; }
-        public Guid Id { get; }
     }
 }
