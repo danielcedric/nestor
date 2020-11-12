63be86f5d58151908619cfc4764a6d6162788dc4
cdaniel <cedric.daniel@outlook.com>
2020-11-12 21:46:06 +0100

    Modification de la structure du pattern et ajout d'un paramètre pour prendre le pattern en entrée

--- Nestor.Tools.Domain/Helpers/NamespaceHelper.cs
+++ Nestor.Tools.Domain/Helpers/NamespaceHelper.cs
@@ -4,17 +4,17 @@ namespace Nestor.Tools.Domain.Helpers
 {
     public static class NamespaceHelper
     {
-        public const string DOMAIN_NAMESPACE_PATTERN = "(?<=Business.).*?(?=.Domain)";
+        public const string DOMAIN_NAMESPACE_PATTERN = "(?<=DataCrafter.).*?(?=.Domain)";// "(?<=Business.).*?(?=.Domain)";
 
         /// <summary>
         ///     Extrait le nom du domaine depuis l'espace de nom
         /// </summary>
         /// <param name="ns"></param>
         /// <returns></returns>
-        public static string ExtractSchemaFromDomain(this string ns)
+        public static string ExtractSchemaFromDomain(this string ns, string domainNamespacePattern = "(?<=DataCrafter.).*?(?=.Domain)")
         {
-            if (Regex.IsMatch(ns, DOMAIN_NAMESPACE_PATTERN))
-                return Regex.Match(ns, DOMAIN_NAMESPACE_PATTERN).Value.ToLowerInvariant();
+            if (Regex.IsMatch(ns, domainNamespacePattern??DOMAIN_NAMESPACE_PATTERN))
+                return Regex.Match(ns, domainNamespacePattern ?? DOMAIN_NAMESPACE_PATTERN).Value.ToLowerInvariant();
 
             return "dbo";
         }
